a1afcbaf47323adc2866aaf07711c0e9fa3c9c24
Manuel Sanchez <manuelsanchez1100@gmail.com>
2017-02-05 19:51:29 -0800

    blog item and urls

---
+++ server/scrapy/engineeringblogs/engineeringblogs/spiders/engineering_blogs.py
@@ -0,0 +1,19 @@
+import scrapy
+from scrapy.selector import Selector
+from ..items import EngineeringblogsItem
+
+
+class BlogSpider(scrapy.Spider):
+  name = 'blogs'
+
+  start_urls = [
+      'https://eng.uber.com/',
+  ]
+
+  def parse(self, response):
+
+    sel = Selector(response)
+
+    titles = sel.css('.post_link').xpath('./a/text()').extract()
+    print titles
+    return [EngineeringblogsItem(title=title) for title in titles]
