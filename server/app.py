2b01333d19bfdd0da5a52a6990031dfb581fe5cb
Manuel Sanchez <manuelsanchez1100@gmail.com>
2017-02-11 22:30:31 -0800

    Switch to flask-restful

--- server/app.py
+++ server/app.py
@@ -1,10 +1,27 @@
-from flask import Flask
-from index import app
+from index import app, db, api
+from flask import Flask, jsonify, request
+from flask_restful import Resource, reqparse
+from models import Blog
+
+parser = reqparse.RequestParser()
+parser.add_argument('title')
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+class EngineeringBlog(Resource):
+
+    def get(self):
+        blogs = Blog.query.all()
+        return jsonify({'blogs': [blog.to_dict() for blog in blogs]})
+
+    def post(self):
+        args = parser.parse_args()
+        blog = Blog(args['title'])
+        db.session.add(blog)
+        db.session.commit()
+        return 1, 201
+
+
+api.add_resource(EngineeringBlog, '/blogs')
 
 
 if __name__ == "__main__":
--- server/index.py
+++ server/index.py
@@ -1,7 +1,9 @@
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from config import BaseConfig
+from flask_restful import Api
 
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
 db = SQLAlchemy(app)
+api = Api(app)
