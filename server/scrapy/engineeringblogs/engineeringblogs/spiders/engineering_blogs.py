6d54ea859a39f20d3cd19fe8d0ed641d7f94f237
Manuel Sanchez <manuelsanchez1100@gmail.com>
2017-02-12 18:28:49 -0800

    Create blog item by following multiple links

--- server/scrapy/engineeringblogs/engineeringblogs/spiders/engineering_blogs.py
+++ server/scrapy/engineeringblogs/engineeringblogs/spiders/engineering_blogs.py
@@ -11,9 +11,12 @@ class BlogSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+        post = response.css('.post')
+        if post is not None:
+            title = post.css('.post-title::text').extract()
+            content = post.css('.post-content p span::text').extract()
+            yield EngineeringblogsItem(title=title, content=content)
 
-        sel = Selector(response)
-
-        titles = sel.css('.post_link').xpath('./a/text()').extract()
-        print titles
-        return [EngineeringblogsItem(title=title) for title in titles]
+        links = response.css('.post_link a::attr(href)').extract()
+        for link in links:
+            yield scrapy.Request(link, callback=self.parse)
