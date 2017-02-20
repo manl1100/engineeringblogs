551f3c9361c96c8c3dc658b2adb28ff0abe68761
Manuel Sanchez <manuelsanchez1100@gmail.com>
2017-02-19 21:56:51 -0800

    Integrate flask-migrate extension and CORS

--- server/index.py
+++ server/index.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import BaseConfig
 from flask_restful import Api
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 api = Api(app)
+CORS(app)
