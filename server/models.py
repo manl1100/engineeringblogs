d4eeb0508a7e62f8bf8d4c52bf3ed43c9bbbb155
Manuel Sanchez <manuelsanchez1100@gmail.com>
2017-02-19 21:54:27 -0800

    Fixed column length for blog title and constructor

--- server/models.py
+++ server/models.py
@@ -6,13 +6,16 @@ class Blog(db.Model):
     __tablename__ = 'blogs'
 
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300))
     company = db.Column(db.String(25))
-    title = db.Column(db.String(100))
     url = db.Column(db.String(300))
     thumbnail = db.Column(db.String(300))
 
-    def __init__(self, title):
+    def __init__(self, title, company, url, thumbnail):
         self.title = title
+        self.company = company
+        self.url = url
+        self.thumbnail = thumbnail
 
     def to_dict(self):
         return {
