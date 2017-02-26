0208f75d962ac4b7b241f1df4e3af08162f7f423
Manuel Sanchez <manuelsanchez1100@gmail.com>
2017-02-26 01:26:38 -0800

    Remove unused POST

--- server/app.py
+++ server/app.py
@@ -5,22 +5,11 @@ from models import Blog
 from schema import blogs_schema
 
 
-parser = reqparse.RequestParser()
-parser.add_argument('title')
-
-
 class EngineeringBlog(Resource):
 
     def get(self):
         blogs = Blog.query.all()
         return blogs_schema.dump(blogs, many=True).data
-
-    def post(self):
-        args = parser.parse_args()
-        blog = Blog(args['title'])
-        db.session.add(blog)
-        db.session.commit()
-        return 1, 201
 
 
 api.add_resource(EngineeringBlog, '/blogs')
